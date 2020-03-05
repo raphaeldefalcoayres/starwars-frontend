fad8a53af90c52f17f2aaa5e348d6d6df4295b55
raphaeldefalcoayres <raphaeldefalcoayres@gmail.com>
2020-03-05 00:51:54 -0300

    basic list characters and initial component paginate

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { MdSearch } from 'react-icons/md';
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import api from '~/services/api';
 
-import { Container, Header, Search, CharacterList, Card } from './styles';
+import {
+  Container,
+  Header,
+  Search,
+  CharacterList,
+  Card,
+  Paginate,
+} from './styles';
 
 export default function Home() {
+  const [characters, setCharacters] = useState([]);
+  const [countCharacters, setCountCharacters] = useState([]);
+
+  async function loadCharacters() {
+    const response = await api.get('people');
+
+    setCountCharacters(response.data.count);
+
+    const data = response.data.results.map(character => {
+      const datetimeFormatted = format(
+        parseISO(character.created),
+        "d 'de' MMMM, 'às' HH'h'",
+        {
+          locale: pt,
+        }
+      );
+      return {
+        ...character,
+        datetimeFormatted,
+      };
+    });
+
+    setCharacters(data);
+  }
+
+  useEffect(() => {
+    loadCharacters();
+  }, []);
+
   return (
     <Container className="container h-full mx-auto py-5">
-      <Header class="w-full">
-        <h1>Startwars character list (0)</h1>
+      <Header className="w-full">
+        <h1>
+          Startwars character list - showing {characters.length || 0} of{' '}
+          {countCharacters || 0}{' '}
+        </h1>
         <Search className="w-1/3">
           <div className="input-group">
             <div className="flex -mr-px">
@@ -30,128 +72,42 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td className="px-4 py-1">1</td>
-              <td className="px-4 py-1">Luke Skywalker</td>
-              <td className="px-4 py-1">2014-12-09T13:50:51.644000Z</td>
-              <td className="px-4 py-1 text-right">
-                <a href="/character/1" className="btn btn-primary">
-                  <MdSearch color="#fff" size="20" />
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td className="px-4 py-1">1</td>
-              <td className="px-4 py-1">Luke Skywalker</td>
-              <td className="px-4 py-1">2014-12-09T13:50:51.644000Z</td>
-              <td className="px-4 py-1 text-right">
-                <a href="/character/1" className="btn btn-primary">
-                  <MdSearch color="#fff" size="20" />
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td className="px-4 py-1">1</td>
-              <td className="px-4 py-1">Luke Skywalker</td>
-              <td className="px-4 py-1">2014-12-09T13:50:51.644000Z</td>
-              <td className="px-4 py-1 text-right">
-                <a href="/character/1" className="btn btn-primary">
-                  <MdSearch color="#fff" size="20" />
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td className="px-4 py-1">1</td>
-              <td className="px-4 py-1">Luke Skywalker</td>
-              <td className="px-4 py-1">2014-12-09T13:50:51.644000Z</td>
-              <td className="px-4 py-1 text-right">
-                <a href="/character/1" className="btn btn-primary">
-                  <MdSearch color="#fff" size="20" />
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td className="px-4 py-1">1</td>
-              <td className="px-4 py-1">Luke Skywalker</td>
-              <td className="px-4 py-1">2014-12-09T13:50:51.644000Z</td>
-              <td className="px-4 py-1 text-right">
-                <a href="/character/1" className="btn btn-primary">
-                  <MdSearch color="#fff" size="20" />
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td className="px-4 py-1">1</td>
-              <td className="px-4 py-1">Luke Skywalker</td>
-              <td className="px-4 py-1">2014-12-09T13:50:51.644000Z</td>
-              <td className="px-4 py-1 text-right">
-                <a href="/character/1" className="btn btn-primary">
-                  <MdSearch color="#fff" size="20" />
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td className="px-4 py-1">1</td>
-              <td className="px-4 py-1">Luke Skywalker</td>
-              <td className="px-4 py-1">2014-12-09T13:50:51.644000Z</td>
-              <td className="px-4 py-1 text-right">
-                <a href="/character/1" className="btn btn-primary">
-                  <MdSearch color="#fff" size="20" />
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td className="px-4 py-1">1</td>
-              <td className="px-4 py-1">Luke Skywalker</td>
-              <td className="px-4 py-1">2014-12-09T13:50:51.644000Z</td>
-              <td className="px-4 py-1 text-right">
-                <a href="/character/1" className="btn btn-primary">
-                  <MdSearch color="#fff" size="20" />
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td className="px-4 py-1">1</td>
-              <td className="px-4 py-1">Luke Skywalker</td>
-              <td className="px-4 py-1">2014-12-09T13:50:51.644000Z</td>
-              <td className="px-4 py-1 text-right">
-                <a href="/character/1" className="btn btn-primary">
-                  <MdSearch color="#fff" size="20" />
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td className="px-4 py-1">1</td>
-              <td className="px-4 py-1">Luke Skywalker</td>
-              <td className="px-4 py-1">2014-12-09T13:50:51.644000Z</td>
-              <td className="px-4 py-1 text-right">
-                <a href="/character/1" className="btn btn-primary">
-                  <MdSearch color="#fff" size="20" />
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td className="px-4 py-1">1</td>
-              <td className="px-4 py-1">Luke Skywalker</td>
-              <td className="px-4 py-1">2014-12-09T13:50:51.644000Z</td>
-              <td className="px-4 py-1 text-right">
-                <a href="/character/1" className="btn btn-primary">
-                  <MdSearch color="#fff" size="20" />
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td className="px-4 py-1">1</td>
-              <td className="px-4 py-1">Luke Skywalker</td>
-              <td className="px-4 py-1">2014-12-09T13:50:51.644000Z</td>
-              <td className="px-4 py-1 text-right">
-                <a href="/character/1" className="btn btn-primary">
-                  <MdSearch color="#fff" size="20" />
-                </a>
-              </td>
-            </tr>
+            {characters.map((character, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{character.name}</td>
+                <td>{character.datetimeFormatted}</td>
+
+                <td className="px-4 py-1 text-right">
+                  <a href="/character/1" className="btn btn-primary">
+                    <MdSearch color="#fff" size="20" />
+                  </a>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </CharacterList>
+        <Paginate className="paginate">
+          <ul>
+            <li>
+              <a href="#">Previous</a>
+            </li>
+            <li>
+              <a href="#">1</a>
+            </li>
+            <li>
+              <a href="#">2</a>
+            </li>
+            <li>
+              <a className="active" href="#">
+                3
+              </a>
+            </li>
+            <li>
+              <a href="#">Next</a>
+            </li>
+          </ul>
+        </Paginate>
       </Card>
     </Container>
   );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -11,3 +11,8 @@ export const Header = styled.header`
 export const Search = styled.div``;
 export const CharacterList = styled.table``;
 export const Card = styled.div``;
+export const Paginate = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 10px;
+`;
