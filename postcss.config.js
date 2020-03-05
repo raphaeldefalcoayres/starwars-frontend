f6d40b47a7ac21b7d7e73016e912671a779c121a
raphaeldefalcoayres <raphaeldefalcoayres@gmail.com>
2020-03-04 23:04:18 -0300

    config postcss + tailwindcss basic with purgecss,cssnano etc for otimization sizes

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./**/*.html'],
+
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    purgecss,
+    require('cssnano'),
+    require('autoprefixer'),
+  ],
+};
